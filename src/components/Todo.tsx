4f29ca8812427b43fa2e95f16ca34500c44ea2d8
jonathan-harty <jonathanharty99@gmail.com>
2020-09-09 10:50:26 -0500

    center list

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ITodo } from "../todos";
+import styles from "../styles/Todo.module.css";
 
 interface ITodoProps {
   todo: ITodo;
@@ -7,8 +8,12 @@ interface ITodoProps {
 
 export const Todo = (props: ITodoProps) => {
   return (
-    <div className="todo">
-      <input type="checkbox" defaultChecked={props.todo.isCompleted} />
+    <div className={styles.todo}>
+      <input
+        className={styles.checkbox}
+        type="checkbox"
+        defaultChecked={props.todo.isCompleted}
+      />
       <h2>{props.todo.title}</h2>
     </div>
   );
